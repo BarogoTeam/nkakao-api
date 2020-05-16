b07675cf0660c930881f024e1f4dc9670b939bdc
Thesun <thesun4sky@gmail.com>
2020-05-16 16:00:05 +0900

    Entity수정 - lombok 어노테이션 적용

--- src/main/java/com/barogo/nkakao/entity/Room.java
+++ src/main/java/com/barogo/nkakao/entity/Room.java
@@ -1,53 +1,24 @@
 package com.barogo.nkakao.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import springfox.documentation.spring.web.json.Json;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@lombok.ToString
 public class Room {
     private String id;
     private List<String> users;
     private String lastChatId;
     private Json info;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public List<String> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<String> users) {
-        this.users = users;
-    }
-
-    public String getLastChatId() {
-        return lastChatId;
-    }
-
-    public void setLastChatId(String lastChatId) {
-        this.lastChatId = lastChatId;
-    }
-
-    public Json getInfo() {
-        return info;
-    }
-
-    public void setInfo(Json info) {
-        this.info = info;
-    }
-
-    public Room(String id, List<String> users, String lastChatId, Json info) {
-        this.id = id;
-        this.users = users;
-        this.lastChatId = lastChatId;
-        this.info = info;
-    }
 
     public Room(String id, List<String> users) {
         this.id = id;
--- src/main/java/com/barogo/nkakao/entity/Token.java
+++ src/main/java/com/barogo/nkakao/entity/Token.java
@@ -1,49 +1,21 @@
 package com.barogo.nkakao.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@lombok.ToString
 public class Token {
     private String userId;
     private String token;
     private Date createDate;
     private Date expireDate;
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getExpireDate() {
-        return expireDate;
-    }
-
-    public void setExpireDate(Date expireDate) {
-        this.expireDate = expireDate;
-    }
-
-    public Token(String userId, String token, Date createDate, Date expireDate) {
-        this.userId = userId;
-        this.token = token;
-        this.createDate = createDate;
-        this.expireDate = expireDate;
-    }
 }
