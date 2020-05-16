4f8d144c92d99f521a86c5eef2a07477de3ff184
Thesun <thesun4sky@gmail.com>
2020-05-16 15:59:24 +0900

    ElasticSearch (Message) - 연동

--- src/main/java/com/barogo/nkakao/entity/Message.java
+++ src/main/java/com/barogo/nkakao/entity/Message.java
@@ -1,59 +1,22 @@
 package com.barogo.nkakao.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@lombok.ToString
+@Document(indexName = "nkakao", type = "message")
 public class Message {
-    private String id;
+    @Id
+    private Integer id;
     private String roomId;
     private String userId;
     private Date date;
     private String msg;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public Message(String id, String roomId, String userId, Date date, String msg) {
-        this.id = id;
-        this.roomId = roomId;
-        this.userId = userId;
-        this.date = date;
-        this.msg = msg;
-    }
 }
